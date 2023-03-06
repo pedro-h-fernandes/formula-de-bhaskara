236a9bae95b063506ed38fcf5bae544b5d3a4d69
Pedro Fernandes <pfernandes180103@gmail.com>
2023-03-06 12:05:04 -0300

    terminando a logica da conta e inserindo os elementos no html

--- index.js
+++ index.js
@@ -1,45 +1,61 @@
-const inputA = document.getElementById('inputA');
-const inputB = document.getElementById('inputB');
-const inputC = document.getElementById('inputC');
 const btn = document.querySelector('.btn');
+const resultadoDiv = document.querySelector('.resultado');
+const paragrafoDiv = document.querySelector('.paragrafos');
+const x1Span = document.querySelector('.x1');
+const x2Span = document.querySelector('.x2');
+const regras = document.querySelector('.regras p');
 
-const valorA = inputA.value;
-const valorB = inputB.value;
-const valorC = inputC.value;
 
 
 btn.addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log('valores input', valorA, valorB, valorC)
-    // bhaskara(valorA, valorB, valorC)
+    e.preventDefault();
 
+    const valorA = document.getElementById('inputA').value;
+    const valorB = document.getElementById('inputB').value;
+    const valorC = document.getElementById('inputC').value;
+
+    bhaskara(valorA, valorB, valorC)
+
+    resultadoDiv.style.visibility = 'visible';
 })
 
 
 
-function bhaskara (a, b, c) {
-    const delta = b**2 - 4 * a * c
+function bhaskara(a, b, c) {
+    const delta = b ** 2 - 4 * a * c
 
-    if (delta > 0 ) {
-        console.log('delta', delta)
+    if (delta > 0) {
 
-        const x1 = -(b) + delta**0.5 / 2 * a;
-        const x2 = -(b) - delta**0.5 / 2 * a;
+        const x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        const x2 = (-b - Math.sqrt(delta)) / (2 * a);
 
         const raizes = [Number.parseInt(x1), Number.parseInt(x2)];
 
-        console.log(`x1 = ${Number.parseInt(x1)}  x2 = ${Number.parseInt(x2)}`)
+        x1Span.textContent = Number.parseInt(x1).toString();
+        x2Span.textContent = Number.parseInt(x2).toString();
+
 
         raizes.forEach(raiz => {
-            if (raiz < 0 ) {
-                console.log (`a raiz ${raiz} é negativa, logo não é considerada.`)
+            if (raiz < 0) {
+                regras.textContent = `A raiz ${raiz} é negativa, logo não é considerada.`;
             }
         })
 
 
     } else {
-        console.log('esta equação nao possui raizes')
+        paragrafoDiv.textContent = 'Δ < 0. Esta equação não possui raizes'
+        regras.textContent = ''
     }
 }
 
-// bhaskara(12, 20, 2)
+//tentativa de colocar a formula
+
+// function addFormula(a, b, c) {
+
+//     const formula = document.createElement('div.formula');
+
+//     formula.appendChild(`<h2>${a}X<sup>2</sup>${b < 0 ? b : '-', b}X ${c < 0 ? c : '-', c} =  0</h2>`)
+
+// }
+
+
